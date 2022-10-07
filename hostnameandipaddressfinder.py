a1e4266e2f70c834ae8547f39f10d830e3577bd8
itzYAGAMI <106914469+itzYAGAMI@users.noreply.github.com>
2022-10-07 17:07:00 +0530

    Update hostnameandipaddressfinder.py

--- hostnameandipaddressfinder.py
+++ hostnameandipaddressfinder.py
@@ -1,18 +1,22 @@
 import socket
-import platform #importing socketing and platform
-
-def hostname_ipaddress_socket():#defining a function for getting hostname and ip by using socket	
-	try:
-		name = socket.gethostname()
-        ip = socket.gethostbyname(socket.gethostname)
+import platform 
+#Function for getting the socket
+def hostname_ipaddress_socket():
+    try:
+        name = socket.gethostname()
+        ip = socket.gethostbyname(name)
     except:
-    	print("There was an error, unable to find Host name and ip address")
+        print("There was an error, unable to find Host name and ip address")
+    else:
+        print(name,ip)
 
-def hostname_ipaddress_platform():#defining a function for getting hostname by using platform
-	try:
-		name = platform.node()
-	except:
-        print("There was an error, unable to find Host name")
+def hostname_ipaddress_platform():
+    try:
+        name = platform.node()
+    except:
+        print("There was an error, unable to find Host name and ip address")
+    else:
+        print(name)
 
 print("------------------MENU---------------------")
 print("1.Host name and ip address by using socket")
@@ -21,21 +25,13 @@ print("2.Host name and ip address by using platform")
 
 ch = int(input(print("Enter your choice: ")))
 
-if !(ch == 1 or ch == 2):
-	print("Enter a valid choice: ")
-	ch = int(input(print("Enter your choice: ")))
+if not ch == 1 or not ch == 2:
+    print("Enter a valid choice: ")
+    ch = int(input("Enter again"))
 
-if ch == 1:#checking which function to use
-	hostname_ipaddress_socket()
+if ch == 1:
+    hostname_ipaddress_socket()
 
 if ch == 2:
-	hostname_ipaddress_socket()
-	hostname_ipaddress_platform()
-
-print("The hostname of this system is: ", end = " ")
-print(name)
-print("The ip address of this system is: ", end = " ")
-print(ip)
-
-
-
+    hostname_ipaddress_socket()
+    hostname_ipaddress_platform()
